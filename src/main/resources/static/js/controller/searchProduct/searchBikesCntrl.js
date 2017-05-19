22240e631340bf95f9c608dcb08a0d784e63009a
kvolotc <meloman-for@mail.ru>
2017-05-19 14:53:39 +0300

    added epty fields

--- src/main/resources/static/js/controller/searchProduct/searchBikesCntrl.js
+++ src/main/resources/static/js/controller/searchProduct/searchBikesCntrl.js
@@ -4,6 +4,8 @@ angular.module('myApp').controller(
 				paginationService) {
 
 			$scope.bikes = [];
+			
+			$scope.isEmpty = false;
 
 			$scope.paginationService = paginationService;
 
@@ -42,6 +44,9 @@ angular.module('myApp').controller(
 								+ $routeParams.page
 					}).then(function(response) {
 				$scope.bikes = response.data;
+				if($scope.bikes.length == 0) {
+					$scope.isEmpty = true;
+				}
 			}, function myError(response) {
 
 			});
